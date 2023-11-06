4cffc13cac66869905989b46a46794a482ae478b
Oleksandr Glushchenko <117204585+LexGlu@users.noreply.github.com>
2023-11-06 14:59:49 +0200

    Creates basic REST API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // this will remove any properties that are not in the DTO
+    transform: true, // this will transform the incoming data to the DTO type
+    forbidNonWhitelisted: true, // this will throw an error if there are properties that are not in the DTO
+  }));
   await app.listen(3000);
 }
 bootstrap();
