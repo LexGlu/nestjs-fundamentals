ea3bca1ad3afec453c0305133ecaa41d8a3581a9
Oleksandr Glushchenko <117204585+LexGlu@users.noreply.github.com>
2023-11-07 11:38:38 +0200

    Adds cascading inserts and updates

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,7 +1,7 @@
 import {Entity, PrimaryGeneratedColumn, Column, JoinTable, ManyToMany} from "typeorm";
 import {Flavor} from "./flavor.entity";
 
-@Entity()
+@Entity() // SQL table === 'coffee'
 export class Coffee {
   @PrimaryGeneratedColumn()
   id: number;
@@ -15,6 +15,10 @@ export class Coffee {
   @JoinTable()
   @ManyToMany(
       type => Flavor,
-      flavor => flavor.coffees)
-  flavors: string[];
+      flavor => flavor.coffees,
+      {
+          cascade: true, // ['insert']
+      },
+  )
+  flavors: Flavor[];
 }
