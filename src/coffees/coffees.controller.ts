a17fde24b55d1db542d95a78b77fde0d63b6bb68
Oleksandr Glushchenko <117204585+LexGlu@users.noreply.github.com>
2023-11-08 18:31:09 +0200

    Adds custom exception filter

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Get, Param, Post, Patch, Delete, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Patch, Delete, Query, UsePipes, ValidationPipe } from "@nestjs/common";
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from "./dto/create-coffee.dto/create-coffee.dto";
 import { UpdateCoffeeDto } from "./dto/update-coffee.dto/update-coffee.dto";
 import { PaginationQueryDto } from "../common/dto/pagination-query.dto/pagination-query.dto";
 
+@UsePipes(ValidationPipe)
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get('')
   findAll(@Query() paginationQuery: PaginationQueryDto) {
     return this.coffeesService.findAll(paginationQuery);
@@ -22,8 +24,9 @@ export class CoffeesController {
     return this.coffeesService.create(createCoffeeDto);
   }
 
+
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+  update(@Param('id') id: string, @Body(ValidationPipe) updateCoffeeDto: UpdateCoffeeDto) {
     return this.coffeesService.update(id, updateCoffeeDto);
   }
 
