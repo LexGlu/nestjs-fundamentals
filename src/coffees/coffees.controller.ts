547b8dbe64ebfde27f416c252967a62cc772152d
Oleksandr Glushchenko <117204585+LexGlu@users.noreply.github.com>
2023-11-10 15:54:20 +0200

    Adds tags to group resources

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -17,8 +17,9 @@ import { PaginationQueryDto } from "../common/dto/pagination-query.dto/paginatio
 import { Public } from "../common/decorators/public.decorator";
 import { ParseIntPipe } from "../common/pipes/parse-int/parse-int.pipe";
 import { Protocol } from "../common/decorators/protocol.decorator";
-import { ApiForbiddenResponse } from "@nestjs/swagger";
+import { ApiForbiddenResponse, ApiTags } from "@nestjs/swagger";
 
+@ApiTags('coffees')
 @UsePipes(ValidationPipe)
 @Controller('coffees')
 export class CoffeesController {
